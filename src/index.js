be9f70a4d5f112fcdc1facbf1015350dcc91a37b
Addison Grant <addison@ifixit.com>
2020-02-01 13:49:10 -0800

    Add the hammer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import Phaser from "phaser";
+import hammerImg from "./assets/hammer.png";
 
 const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
+  physics: {
+      default: 'matter',
+      matter: {
+          debug: true,
+          gravity: {
+              y: 1
+          },
+      }
+  },
   scene: {
     preload: preload,
     create: create
@@ -13,7 +23,18 @@ const config = {
 const game = new Phaser.Game(config);
 
 function preload() {
+  this.load.image("hammer", hammerImg);
 }
 
 function create() {
+
+  this.matter.world.setBounds();
+  var handle = this.matter.add.circle(200, 300, 10);
+  var hammer = this.matter.add.image(400, 300, 'hammer');
+  this.matter.add.constraint(handle, hammer, 150, 0.5, {
+    pointA: new Phaser.Geom.Point(200, 300)
+  });
+
+  this.matter.add.mouseSpring();
+
 }
